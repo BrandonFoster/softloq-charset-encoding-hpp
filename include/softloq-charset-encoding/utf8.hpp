a47ed75dcb53186e80fa36cbd60fcc30dc825819
Brandon Foster <Brandon.Foster4525@outlook.com>
2024-12-07 09:13:13 -0500

    Add basic setup

---
+++ include/softloq-charset-encoding/utf8.hpp
@@ -0,0 +1,11 @@
+#ifndef SOFTLOQ_CHARSET_ENCODING_UTF8_HPP
+#define SOFTLOQ_CHARSET_ENCODING_UTF8_HPP
+
+#include "softloq-charset-encoding/error.hpp"
+
+namespace Softloq::Charset
+{
+
+}
+
+#endif
---
+++ tests/example.cpp
@@ -0,0 +1,6 @@
+#include <catch2/catch_test_macros.hpp>
+#include <softloq-charset-encoding/utf8.hpp>
+
+TEST_CASE("UTF-8 Charset Encoding", "[utf8]")
+{
+}
