191c726661d2b192a8fa2efe79d08aec27af9693
Brandon Foster <Brandon.Foster4525@outlook.com>
2024-12-09 03:49:39 -0500

    case change

---
+++ include/Softloq-Charset-Encoding/decoder.hpp
@@ -0,0 +1,11 @@
+#ifndef SOFTLOQ_CHARSET_ENCODING_DECODER_HPP
+#define SOFTLOQ_CHARSET_ENCODING_DECODER_HPP
+
+#include "Softloq-Charset-Encoding/error.hpp"
+
+namespace Softloq::Charset
+{
+
+}
+
+#endif
---
+++ include/Softloq-Charset-Encoding/utf8.hpp
@@ -0,0 +1,11 @@
+#ifndef SOFTLOQ_CHARSET_ENCODING_UTF8_HPP
+#define SOFTLOQ_CHARSET_ENCODING_UTF8_HPP
+
+#include "Softloq-Charset-Encoding/error.hpp"
+
+namespace Softloq::Charset
+{
+
+}
+
+#endif
